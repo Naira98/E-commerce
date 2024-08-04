f1f1f005c893fec082f6c560bf53e5eb69b5f6ed
Naira98 <nairamm99@gmail.com>
2024-08-04 15:05:50 +0300

    adding favicon

--- assignment/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usersData = require('./form');
-
-router.get('/users', (req, res, next) => {
-    res.render('users', {pageTitle: 'Users', users: usersData.usernames});
-})
-
-exports.routes = router;
